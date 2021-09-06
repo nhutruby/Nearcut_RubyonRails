601ff752a5d35ce0e2a6e1b5ccedded7ed002fb3
Benerik <benerik.nguyen@gmail.com>
2021-09-06 16:28:46 +0700

    deploy on heroku

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+ENV["MONGODB_URI"] = "mongodb://iad2-c16-1.mongo.objectrocket.com:52946,iad2-c16-2.mongo.objectrocket.com:52946,iad2-c16-0.mongo.objectrocket.com:52946/?replicaSet=0d3fd1ed15144b16a3c518c7a838ce74&ssl=true"
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
